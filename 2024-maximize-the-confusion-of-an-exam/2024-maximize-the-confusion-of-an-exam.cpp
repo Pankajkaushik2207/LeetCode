e70b69e39b2ed7dade1cb38db59437e6bf230fe8
Pankaj kaushik <121494366+Pankajkaushik2207@users.noreply.github.com>
2023-07-07 22:04:45 +0530

    Time: 15 ms (99.65%), Space: 10.1 MB (59.24%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+  int maxConsecutiveAnswers(string answerKey, int k) {
+    int n=answerKey.size(), cntT=0, cntF=0;
+    int l=0, r=0, maxi=INT_MIN;
+    
+    while(r<n){
+        if(answerKey[r]=='T') cntT++;
+        else cntF++;
+        
+        while(cntT>k && cntF>k){
+            if(answerKey[l]=='T') cntT--;
+            else cntF--;
+            l++;
+        }
+        
+        maxi=max(maxi, r-l+1);
+        r++;
+    }
+    
+    return maxi;  
+}
+};
