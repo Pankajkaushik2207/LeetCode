4960592003d04f49604272188aa565c583e2804f
Pankaj kaushik <121494366+Pankajkaushik2207@users.noreply.github.com>
2023-07-25 11:26:25 +0530

    Time: 121 ms (77.28%), Space: 59.6 MB (40.39%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-          priority_queue<int>mh;
-          int m;
-          int i=0;
+//           priority_queue<int>mh;
+//           int m;
+//           int i=0;
         
-        for(;i<arr.size();i++){
-            mh.push(arr[i]);
-            if(arr[i]==mh.top()){
-              m=i;
-           }
+//         for(;i<arr.size();i++){
+//             mh.push(arr[i]);
+//             if(arr[i]==mh.top()){
+//               m=i;
+//            }
+//         }
+         
+         
+//         return m;
+        int l=0, r= arr.size()-1, mid;
+        while(l<r){
+            mid   =(l+r)/2;
+            if(arr[mid]<arr[mid+1]){
+                l= mid+1;
+            }else{
+                
+                r=mid;
+            }
+            
         }
-         
-         
-        return m;
-        
+        return l;
     }
 };
