854cc7d22712bc61ed7d45332bf3109b1f950dd8
Pankaj kaushik <pankajkaushik042@gmail.com>
2023-07-30 16:53:22 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (6.91%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -7,8 +7,24 @@ public:
         int ans=fib(n-1)+fib(n-2);
         return ans;
     }
+    
+    int topDown(int n, vector<int> &dp){
+       
+        
+        if(n==0||n==1){
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+       dp[n] = topDown(n-1,dp)+topDown(n-2,dp);
+        return dp[n];
+    }
     int fib(int n) {
-        int ans= recSol(n);
+        // int ans= recSol(n);
+        // return ans;
+        vector<int>dp(n+1,-1);
+        int ans = topDown(n,dp);
         return ans;
     }
 };
