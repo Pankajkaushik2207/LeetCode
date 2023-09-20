a53952c19437238b6499bbca5409d95999dce661
Pankaj kaushik <pankajkaushik042@gmail.com>
2023-09-20 22:52:34 +0530

    Time: 303 ms (25.09%), Space: 154 MB (17.53%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,23 +1,23 @@
 #include <vector>
 #include <unordered_map>
-#include <numeric>
 #include <climits>
 
 class Solution {
 public:
     int minOperations(std::vector<int>& nums, int x) {
-     int target = std::accumulate(nums.begin(), nums.end(), 0LL) - static_cast<long long>(x);
+        int n = nums.size();
+        int target = std::accumulate(nums.begin(), nums.end(), 0LL) - static_cast<long long>(x);
 
         if (target == 0) {
-            return nums.size(); // The entire array should be removed.
+            return n; // The entire array should be removed.
         }
-        
+
         std::unordered_map<int, int> prefixSum;
         prefixSum[0] = -1;
         int sum = 0;
         int maxLength = INT_MIN;
-        
-        for (int i = 0; i < nums.size(); ++i) {
+
+        for (int i = 0; i < n; ++i) {
             sum += nums[i];
             if (prefixSum.find(sum - target) != prefixSum.end()) {
                 int subarrayLength = i - prefixSum[sum - target];
@@ -25,11 +25,7 @@ public:
             }
             prefixSum[sum] = i;
         }
-        
-        if (maxLength == INT_MIN) {
-            return -1; // No subarray found.
-        }
-        
-        return nums.size() - maxLength;
+
+        return maxLength != INT_MIN ? n - maxLength : -1;
     }
 };
