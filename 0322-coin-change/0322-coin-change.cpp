6e783f4ecbb83c980104f2eda2482ce8617b1a27
Pankaj kaushik <pankajkaushik042@gmail.com>
2023-10-21 12:09:45 +0530

    Time: 62 ms (73.05%), Space: 15.8 MB (47.94%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,59 +1,53 @@
 class Solution {
 public:
-    int Recursion(vector<int>&coins,int amount){
-        if(amount==0){
+    // int recursion(vector<int>&coins,int amount){
+    //      if(amount ==0){
+    //         return 0;
+    //     }
+    //     if(amount<0){
+    //         return INT_MAX;
+    //     }
+    //     int mini =INT_MAX;
+    //     for(int i=0;i<coins.size();i++){
+    //         int ans = recursion(coins,amount-coins[i]);
+    //         if(ans!=INT_MAX){
+    //             mini= min(mini,ans+1);
+    //         }
+    //     }
+    //     return mini;
+    // }
+    
+    
+     int recursion(vector<int>&coins,int amount,vector<int>&dp){
+         if(amount ==0){
             return 0;
         }
-        if(amount <0){
+        if(amount<0){
             return INT_MAX;
         }
-        int mini = INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            int ans =Recursion(coins,amount-coins[i]);
-         if(ans!=INT_MAX){
-            mini= min(mini,1+ans);
+         if(dp[amount]!=-1){
+             return dp[amount];
          }
-            
-        }
-        return mini;
-        }
-    int topDown(vector<int>&coins,int amount,vector<int>&dp){
-        if(amount==0){
-            return 0;
-        }
-        if(amount <0){
-            return INT_MAX;
-        }
-//         check whether amount is already exist or not
-     if(dp[amount]!=-1){
-        return dp[amount];
-     }
-        int mini = INT_MAX;
+        int mini =INT_MAX;
         for(int i=0;i<coins.size();i++){
-            int ans =topDown(coins,amount-coins[i],dp);
-         if(ans!=INT_MAX){
-            mini= min(mini,1+ans);
-         }
-            
+            int ans = recursion(coins,amount-coins[i],dp);
+            if(ans!=INT_MAX){
+                mini= min(mini,ans+1);
+            }
         }
-        dp[amount]= mini;
+         dp[amount] = mini;
         return mini;
     }
+    
+    
     int coinChange(vector<int>& coins, int amount) {
-//         int ans = Recursion(coins,amount);
-//         if(ans==INT_MAX){
-//             return -1;
-            
-//         }else{
-//         return ans;
-//         }
         vector<int>dp(amount+1,-1);
-        int ans = topDown(coins,amount,dp);
+        int ans = recursion(coins,amount,dp);
         if(ans==INT_MAX){
             return -1;
-        }
-        else{
+        }else{
             return ans;
         }
     }
+    
 };
